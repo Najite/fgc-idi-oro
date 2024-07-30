9d21fa08350b68dfabf6bafb1f49ed2ec0ef2f1c
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2024-07-30 20:59:52 +0100

    changes

--- server/routes/streamRoute.js
+++ server/routes/streamRoute.js
@@ -4,9 +4,11 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/create", streamController.createStream);
+router.post("/", streamController.createStream);
 router.get("/", streamController.getAllStreams);
-router.delete("/:id", streamController.deleteStream);
+router.delete("/:id", (req, res, next) => {
+    console.log('delete request', req.params.id);
+    next();
+}, streamController.deleteStream);
 
-
-
+module.exports = router
