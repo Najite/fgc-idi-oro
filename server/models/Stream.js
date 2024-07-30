f99ac6c0970da3f4e2b55bf429572b15e0c823d9
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2024-07-30 06:34:07 +0100

    created stream link

---
+++ server/models/Stream.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const streamSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    streamUrl: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: {
+            validator: (v) => {
+                return /^https?:\/\/[^\s$.?#].[^\s]*/.test(v);
+            },
+            message: props => `${props.value} is not a valid URL`
+        }
+    }
+})
+
